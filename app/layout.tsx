f92662ad173e634d36ba3e030f7de91a47dd110d
王微 <buttonww@163.com>
2024-09-12 17:09:12 +0800

    fix googele ads error crossOrigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8356786738935211"
-     crossorigin="anonymous"></script></head>
+     crossOrigin="anonymous"></script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
