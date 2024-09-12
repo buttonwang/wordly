16147108dd50143d10e63f1e3227b9be55c116b2
王微 <buttonww@163.com>
2024-09-12 17:00:09 +0800

    add  google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8356786738935211"
+     crossorigin="anonymous"></script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
